27f54634e4e03dd998a4b022e4c70cc9a6b7b15d
ljxfai <18935174185@163.com>
2020-09-08 15:30:52 +0800

    更改路由

--- vue_test/src/router/index.js
+++ vue_test/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '../components/HelloWorld'
+import Upload from '../components/Recording_and_broadcasting/Upload'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/upload',
+      name: 'Upload',
+      component: Upload
     }
   ]
 })
