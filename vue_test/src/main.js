08ebe0e8f220b6163bb6169e08a07445eafd50d5
mango1o <844462145@qq.com>
2020-09-08 14:35:29 +0800

    el-UI引入以及配置

--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -3,13 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.use(ElementUI);
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+});
+Vue.config.productionTip = false
+
+// /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   components: { App },
+//   template: '<App/>'
+// })
