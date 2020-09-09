3e8697968d8f3f4158af44f2863fae292ecd16fb
mango1o <844462145@qq.com>
2020-09-09 09:51:21 +0800

    登录注册页面路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '../components/HelloWorld'
 import Upload from '../components/Record/Upload'
+import Login from '../components/userManage/login'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/upload',
       name: 'Upload',
       component: Upload
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
