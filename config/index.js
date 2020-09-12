f1c9f67e253091dd75291299dc846270f64b447b
mango1o <844462145@qq.com>
2020-09-12 17:02:00 +0800

    somechanges

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     proxyTable: {
       '/api': {
         target: "http://localhost:8080/api",  //服务器域名
